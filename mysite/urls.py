b848fe890b1b41dcf22abb0c6d7526c31604e966
Raimund Ege <raimund@ege.com>
2019-11-07 17:33:36 -0800

    getting started

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,7 @@ from django.views.generic.base import RedirectView
 favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
-    re_path(r'^favicon\.ico$', favicon_view),
+    path(r'^favicon\.ico$', favicon_view),
     path('encrypt/', include('encrypt.urls')),
     path('admin/', admin.site.urls),
 ]
